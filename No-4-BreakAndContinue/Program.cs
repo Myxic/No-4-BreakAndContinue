d60fae40502b28362b6f2589808b41bd29fa365b
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-09 16:08:36 +0100

    upload

---
+++ No-4-BreakAndContinue/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 0; i < 6; i++)
+{
+    if (i == 4)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+for (int i = 0; i < 6; i++)
+{
+    if (i == 4)
+    {
+        break;
+    }
+    Console.WriteLine(i);
+}
+
